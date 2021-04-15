8921ef84e8a875285ac8f8b5568bd0c8bd58b737
Juan Pablo Galeano S <56057491+JuanGaleano14@users.noreply.github.com>
2021-04-15 18:48:52 -0500

    Pagina de inicio
    Se desarrolla visual del tab de inicio al confirmar el logueo.

--- lib/src/ui/pages/start_page.dart
+++ lib/src/ui/pages/start_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
+
+import '../tab_bar_views/playlist_tabBarView.dart';
+import 'my_library_page.dart';
+import 'my_library_page.dart';
+
+const kBackgroundColor = Color(0xFF202020);
+const kPrimaryColor = Color(0xFF80DEEA);
 
 class StartPage extends StatefulWidget {
   StartPageState createState() => new StartPageState();
@@ -7,10 +15,107 @@ class StartPage extends StatefulWidget {
 class StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      decoration: new BoxDecoration(color: Theme.of(context).primaryColor),
-      child: Center(
-        child: Text("Inicio"),
+    return MaterialApp(
+      title: 'Inicio',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: TextTheme(
+          display1: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          button: TextStyle(color: kPrimaryColor),
+          headline:
+          TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white.withOpacity(.2),
+            ),
+          ),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/tempScreen/prueba6.jpeg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Que no te falte la música\n',
+                        style: Theme.of(context).textTheme.display1,
+                      ),
+                      TextSpan(
+                        text: 'en tus mejores momentos...',
+                        style: Theme.of(context).textTheme.headline,
+                      )
+                    ],
+                  ),
+                ),
+                FittedBox(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) {
+                          return MyLibraryPage();
+                        },
+                      ));
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 22),
+                      padding:
+                      EdgeInsets.symmetric(horizontal: 36, vertical: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: kPrimaryColor,
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            'Echa un vistazo a tus playlists',
+                            style: Theme.of(context).textTheme.button.copyWith(
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.arrow_forward,
+                            color: Colors.black,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
